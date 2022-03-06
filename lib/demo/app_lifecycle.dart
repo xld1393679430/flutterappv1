3fd8a8dcd7f5479f749cbd0cd31e00536c42f04c
ld_xu <ld_xu@trip.com>
2022-03-06 10:50:38 +0800

    flutter 应用的生命周期

--- lib/demo/app_lifecycle.dart
+++ lib/demo/app_lifecycle.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 
 // flutter应用的生命周期
+// 通过绑定WidgetsBindingObserver观察器监听应用的生命周期
 class AppLifecyclePage extends StatefulWidget {
   @override
   _AppLifecyclePageState createState() => _AppLifecyclePageState();
 }
 
-class _AppLifecyclePageState extends State<AppLifecyclePage> {
+class _AppLifecyclePageState extends State<AppLifecyclePage>
+    with WidgetsBindingObserver {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,5 +30,30 @@ class _AppLifecyclePageState extends State<AppLifecyclePage> {
         ),
       ),
     );
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    // super.didChangeAppLifecycleState(state);
+    print("State = $state");
+    if (state == AppLifecycleState.paused) {
+      print("Ap进入后台");
+    } else if (state == AppLifecycleState.resumed) {
+      print("App进入后台");
+    } else if (state == AppLifecycleState.inactive) {
+      print("App进入非激活状态, 比如被电话打断");
+    }
+  }
+
+  @override
+  void initState() {
+    WidgetsBinding.instance.addObserver(this); // 在super之前调用
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this); // 在super之前调用
+    super.dispose();
   }
 }
